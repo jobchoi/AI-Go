7454482536205969f1ed49d8b04639a6944e9041
jobchoi <sedcreta12@gmail.com>
2026-08-18 12:32:08 +0900

    resize cli로 확인 성공, 윈도우에서 확인할 수 있게 작업 시작

--- tests/test_image_input.py
+++ tests/test_image_input.py
@@ -11,6 +11,19 @@ print("이미지 로드 성공")
 print(frame.shape)
 print(f"shape:{frame.shape}")
 
+# 처리할 이미지의 가로 크기
+target_width = 1000
+
+height = int(
+    frame.shape[0] * (target_width / frame.shape[1])
+)
+resized_frame = cv2.resize(
+    frame, 
+    (target_width, height)
+)
+
+print(f"resized shape:{resized_frame.shape}")
+
 # cv2.imshow("AI-Go Sample", frame)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
